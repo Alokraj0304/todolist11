cca71d197114d1bc140422e3164063d32321b1a2
Alokraj0304 <alokraj0304@gmail.com>
2024-12-16 15:56:58 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+  let footerstyle = {
+    position: "relative",
+    top: "10vh",
+    width: "100%"
+  }
+  return (
+    <footer className="bg-dark text-light py-3" style=
+    {footerstyle}>
+      <p className="text-center">
+       Copyright &copy; MyTodoslist.com
+       </p>
+    </footer>
+    
+  )
+}
+
+//export default Footer
